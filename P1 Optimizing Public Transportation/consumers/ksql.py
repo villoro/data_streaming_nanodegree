1c1df4eea7e2c766b89158439194e0a725544ae1
Arnau Villoro <arnau@villoro.com>
2022-03-26 17:12:50 +0100

    Project 1 Submission 1 (#2)
    * Add producers part 1
    
    * improve station and turnstile
    
    * Add weather part
    
    * fix producer adminClient + fix stations
    
    * Allow nulls in prev station/direction
    
    * Improve topic names
    
    * improve topic names 2
    
    * Fix problems in weather
    
    * Implement connector code
    
    * Implement faust_stream
    
    * ksql section
    
    * set consumers
    
    * rename streams
    
    * drop unnecessary loop + fix consumer

--- P1 Optimizing Public Transportation/consumers/ksql.py
+++ P1 Optimizing Public Transportation/consumers/ksql.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 
 KSQL_URL = "http://localhost:8088"
 
-#
 # TODO: Complete the following KSQL statements.
 # TODO: For the first statement, create a `turnstile` table from your turnstile topic.
 #       Make sure to use 'avro' datatype!
@@ -23,14 +22,20 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC='org.chicago.cta.station.turnstile.v1',
+    VALUE_FORMAT='AVRO',
+    KEY='station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT='JSON') AS
+    SELECT station_id, COUNT(*) AS count
+    FROM turnstile
+    GROUP BY station_id;
 """
 
 
--- P1 Optimizing Public Transportation/producers/models/producer.py
+++ P1 Optimizing Public Transportation/producers/models/producer.py
@@ -4,10 +4,14 @@ import time
 
 
 from confluent_kafka import avro
-from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.admin import AdminClient
+from confluent_kafka.admin import NewTopic
 from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
 
 
 class Producer:
@@ -25,23 +29,19 @@ class Producer:
         num_replicas=1,
     ):
         """Initializes a Producer object with basic settings"""
+
         self.topic_name = topic_name
         self.key_schema = key_schema
         self.value_schema = value_schema
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        #
-        #
         # TODO: Configure the broker properties below. Make sure to reference the project README
         # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
-        self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
-        }
+        self.broker_properties = {"bootstrap.servers": BROKER_URL}
+
+        # AdminClient does not need SCHEMA_REGISTRY_URL
+        self.client = AdminClient(self.broker_properties)
 
         # If the topic does not already exist, try to create it
         if self.topic_name not in Producer.existing_topics:
@@ -49,30 +49,44 @@ class Producer:
             Producer.existing_topics.add(self.topic_name)
 
         # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.broker_properties["schema.registry.url"] = SCHEMA_REGISTRY_URL
+        self.producer = AvroProducer(
+            self.broker_properties,
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema,
+        )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
         # TODO: Write code that creates the topic for this producer if it does not already exist on
         # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
 
-    def time_millis(self):
-        return int(round(time.time() * 1000))
+        if self.topic_name in self.client.list_topics().topics:
+            logger.warning(f"Topic '{self.topic_name}' already exists")
+            return
+
+        topic = NewTopic(
+            self.topic_name,
+            num_partitions=self.num_partitions,
+            replication_factor=self.num_replicas,
+        )
+
+        for topic, future in self.client.create_topics([topic]).items():
+            try:
+                future.result()
+            except Exception as e:
+                logger.exception(f"Failed to create topic '{self.topic_name}'")
+                raise e
+            else:
+                logger.info(f"Topic '{self.topic_name}' created")
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
         # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+
+        if self.producer is not None:
+            logger.info(f"Flushing '{self.topic_name}' to close the producer")
+            self.producer.flush()
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
