d569235d16bbdb5ec60fbf490498005d1440bac3
villoro7 <villoro7@gmail.com>
2022-04-04 20:20:31 +0200

    move things into src

---
+++ P2 Evaluate Human Balance with Spark Streaming/src/submit-event-kafkajoin.cmd
@@ -0,0 +1,1 @@
+docker exec -it 6c1ec5d880e0 /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/workspace/src/sparkpykafkajoin.py | tee logs/kafkajoin.log 
--- P2 Evaluate Human Balance with Spark Streaming/submit-redis-kafka-streaming.cmd
+++
@@ -1,1 +0,0 @@
-docker exec -it 158ca42968dc /opt/bitnami/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 /home/workspace/project/villoro/sparkpyrediskafkastreamtoconsole.py | tee logs/redis-kafka.log 
